52c5207b96a40517b99a52a4e1d1477b37f20d2f
project <1433275904@qq.com>
2023-06-08 12:27:08 +0800

    2023/6/8 Beginning 框架搭建

---
+++ rlg-backend/rlg-backend-main/src/main/java/cn/maplerabbit/rlg/RlgBackendMainApplication.java
@@ -0,0 +1,13 @@
+package cn.maplerabbit.rlg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlgBackendMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlgBackendMainApplication.class, args);
+	}
+
+}
---
+++ rlg-backend/rlg-backend-main/src/test/java/cn/maplerabbit/rlg/RlgBackendMainApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.maplerabbit.rlg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlgBackendMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
