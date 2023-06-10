2fde8b8c203570fcefd9ae8d1aba555772b9fe20
project <1433275904@qq.com>
2023-06-10 09:37:59 +0800

    2023/6/10 模块部分优化，接口与实现类拆分为两个子模块 前端Vue部分使用最新Vue3重新构建

---
+++ rlg-backend/rlg-backend-main/src/main/java/cn/maplerabbit/rlg/RlgApplication.java
@@ -0,0 +1,14 @@
+package cn.maplerabbit.rlg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlgApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlgApplication.class, args);
+	}
+
+}
---
+++ rlg-backend/rlg-backend-main/src/test/java/cn/maplerabbit/rlg/RlgApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.maplerabbit.rlg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlgApplicationTests
+{
+
+	@Test
+	void contextLoads() {
+	}
+
+}
